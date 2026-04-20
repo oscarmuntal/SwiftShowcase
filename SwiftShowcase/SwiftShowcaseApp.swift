993deacb719daddec96196f0dd1b478cd9e98bca
Òscar Muntal <oscar.muntal@gmail.com>
2026-04-20 08:43:33 +0200

    Initial Commit

---
+++ SwiftShowcase/SwiftShowcaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftShowcaseApp.swift
+//  SwiftShowcase
+//
+//  Created by Òscar Muntal on 20/4/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftShowcaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
