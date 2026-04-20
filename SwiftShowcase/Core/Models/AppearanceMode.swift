2b76223a8a699d26c29ce45000b2942788f1bc15
Òscar Muntal <oscar.muntal@gmail.com>
2026-04-20 12:06:22 +0200

    feat: add AppearanceMode enum for three-way light/dark/system selection

---
+++ SwiftShowcase/Core/Models/AppearanceMode.swift
@@ -0,0 +1,25 @@
+import SwiftUI
+
+enum AppearanceMode: String, CaseIterable, Identifiable {
+    case system
+    case light
+    case dark
+
+    var id: Self { self }
+
+    var displayName: String {
+        switch self {
+        case .system: "System"
+        case .light: "Light"
+        case .dark: "Dark"
+        }
+    }
+
+    var colorScheme: ColorScheme? {
+        switch self {
+        case .system: nil
+        case .light: .light
+        case .dark: .dark
+        }
+    }
+}
